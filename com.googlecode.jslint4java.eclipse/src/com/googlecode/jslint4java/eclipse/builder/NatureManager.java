513f4d848f826d4894d20737440b9899bd30664e
Dominic Mitchell <dom@happygiraffe.net>
2010-11-22 21:23:57 +0000

    Split out core and UI.

---
+++ com.googlecode.jslint4java.eclipse/src/com/googlecode/jslint4java/eclipse/builder/NatureManager.java
@@ -0,0 +1,48 @@
+package com.googlecode.jslint4java.eclipse.builder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.runtime.CoreException;
+
+import com.googlecode.jslint4java.eclipse.JSLintLog;
+
+/**
+ * Manage adding and removing the nature from a project.
+ */
+public class NatureManager {
+
+    /** Return a mutable list of natures on this project. */
+    private List<String> getNatures(IProject project) throws CoreException {
+        IProjectDescription desc = project.getDescription();
+        return new ArrayList<String>(Arrays.asList(desc.getNatureIds()));
+    }
+
+    /** Apply this list of natures to a project. */
+    private void setNatures(IProject project, List<String> natures) throws CoreException {
+        IProjectDescription desc = project.getDescription();
+        desc.setNatureIds(natures.toArray(new String[natures.size()]));
+        project.setDescription(desc, null);
+    }
+
+    /** Enable or disable the JSLint nature on a project. */
+    public void toggleNature(IProject project) {
+        try {
+            List<String> natures = getNatures(project);
+            if (natures.contains(JSLintNature.NATURE_ID)) {
+                // Remove the nature.
+                natures.remove(JSLintNature.NATURE_ID);
+            } else {
+                // Add the nature.
+                natures.add(JSLintNature.NATURE_ID);
+            }
+            setNatures(project, natures);
+        } catch (CoreException e) {
+            JSLintLog.logError(e);
+        }
+    }
+
+}
--- com.googlecode.jslint4java.eclipse/src/com/googlecode/jslint4java/eclipse/builder/ToggleNatureAction.java
+++ com.googlecode.jslint4java.eclipse/src/com/googlecode/jslint4java/eclipse/builder/ToggleNatureAction.java
@@ -1,29 +1,22 @@
 package com.googlecode.jslint4java.eclipse.builder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IActionDelegate;
 
-import com.googlecode.jslint4java.eclipse.JSLintLog;
-
 public class ToggleNatureAction implements IActionDelegate {
 
     private IStructuredSelection selection;
 
     public void run(IAction action) {
+        NatureManager natureManager = new NatureManager();
         for (Object obj : selection.toArray()) {
             IProject project = projectFromSelectedItem(obj);
             if (project != null) {
-                toggleNature(project);
+                natureManager.toggleNature(project);
             }
         }
     }
@@ -42,30 +35,6 @@ public class ToggleNatureAction implements IActionDelegate {
     public void selectionChanged(IAction action, ISelection selection) {
         if (selection instanceof IStructuredSelection) {
             this.selection = (IStructuredSelection) selection;
-        }
-    }
-
-    /**
-     * Toggles sample nature on a project
-     *
-     * @param project
-     *            to have sample nature added or removed
-     */
-    private void toggleNature(IProject project) {
-        try {
-            IProjectDescription description = project.getDescription();
-            List<String> natures = new ArrayList<String>(Arrays.asList(description.getNatureIds()));
-            if (natures.contains(JSLintNature.NATURE_ID)) {
-                // Remove the nature.
-                natures.remove(JSLintNature.NATURE_ID);
-            } else {
-                // Add the nature.
-                natures.add(JSLintNature.NATURE_ID);
-            }
-            description.setNatureIds(natures.toArray(new String[natures.size()]));
-            project.setDescription(description, null);
-        } catch (CoreException e) {
-            JSLintLog.logError(e);
         }
     }
 
