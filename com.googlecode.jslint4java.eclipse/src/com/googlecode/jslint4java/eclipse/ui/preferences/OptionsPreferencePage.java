de50bfa572e065bc07848e5664f652b3131c0436
Dominic Mitchell <dom@happygiraffe.net>
2010-11-24 23:43:17 +0000

    Get remaining prefs into the prefs page.
    Still ugly as sin. :)

--- com.googlecode.jslint4java.eclipse/src/com/googlecode/jslint4java/eclipse/ui/preferences/OptionsPreferencePage.java
+++ com.googlecode.jslint4java.eclipse/src/com/googlecode/jslint4java/eclipse/ui/preferences/OptionsPreferencePage.java
@@ -1,24 +1,26 @@
 package com.googlecode.jslint4java.eclipse.ui.preferences;
 
-
 import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 import com.googlecode.jslint4java.Option;
+import com.googlecode.jslint4java.StringArray;
 import com.googlecode.jslint4java.eclipse.Activator;
 
 /**
- * This class represents a preference page that is contributed to the
- * Preferences dialog. By subclassing <samp>FieldEditorPreferencePage</samp>,
- * we can use the field support built into JFace that allows us to create a page
- * that is small and knows how to save, restore and apply itself.
+ * This class represents a preference page that is contributed to the Preferences dialog. By
+ * subclassing <samp>FieldEditorPreferencePage</samp>, we can use the field support built into JFace
+ * that allows us to create a page that is small and knows how to save, restore and apply itself.
  * <p>
- * This page is used to modify preferences only. They are stored in the
- * preference store that belongs to the main plug-in class. That way,
- * preferences can be accessed directly via the preference store.
+ * This page is used to modify preferences only. They are stored in the preference store that
+ * belongs to the main plug-in class. That way, preferences can be accessed directly via the
+ * preference store.
  */
 
 public class OptionsPreferencePage extends FieldEditorPreferencePage implements
@@ -31,17 +33,23 @@ public class OptionsPreferencePage extends FieldEditorPreferencePage implements
     }
 
     /**
-     * Creates the field editors. Field editors are abstractions of the common
-     * GUI blocks needed to manipulate various types of preferences. Each field
-     * editor knows how to save and restore itself.
+     * Creates the field editors. Field editors are abstractions of the common GUI blocks needed to
+     * manipulate various types of preferences. Each field editor knows how to save and restore
+     * itself.
      */
     @Override
     public void createFieldEditors() {
         Composite parent = getFieldEditorParent();
         for (Option o : Option.values()) {
+            FieldEditor ed = null;
             if (o.getType() == Boolean.class) {
-                BooleanFieldEditor ed = new BooleanFieldEditor(
-                        o.getLowerName(), o.getDescription(), parent);
+                ed = new BooleanFieldEditor(o.getLowerName(), o.getDescription(), parent);
+            } else if (o.getType() == Integer.class ) {
+                ed = new IntegerFieldEditor(o.getLowerName(), o.getDescription(), parent);
+            } else if (o.getType() == StringArray.class) {
+                ed = new StringFieldEditor(o.getLowerName(), o.getDescription(), parent);
+            }
+            if (ed != null) {
                 addField(ed);
             }
         }
