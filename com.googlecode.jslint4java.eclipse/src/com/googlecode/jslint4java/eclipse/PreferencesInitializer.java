c42ac46176fb2a9c1df9a75131416bef853b7e09
Dominic Mitchell <dom@happygiraffe.net>
2010-11-28 00:46:34 +0000

    Switch from preferences.ini to an initializer.
    Unfortunately it doesn't work.  Yet.

---
+++ com.googlecode.jslint4java.eclipse/src/com/googlecode/jslint4java/eclipse/PreferencesInitializer.java
@@ -0,0 +1,33 @@
+package com.googlecode.jslint4java.eclipse;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+
+import com.googlecode.jslint4java.Option;
+
+/**
+ * Set up the default preferences.  By default,we enable:
+ * <ul>
+ * <li> {@link Option#EQEQEQ}
+ * <li> {@link Option#UNDEF}
+ * <li> {@link Option#WHITE}
+ * <li>
+ */
+public class PreferencesInitializer extends AbstractPreferenceInitializer {
+
+
+    private final Set<Option> defaultEnable = EnumSet.of(Option.EQEQEQ, Option.UNDEF, Option.WHITE);
+
+    @Override
+    public void initializeDefaultPreferences() {
+        IEclipsePreferences node = new DefaultScope().getNode(Activator.PLUGIN_ID);
+        for (Option o : defaultEnable) {
+            node.putBoolean(o.getLowerName(), true);
+        }
+    }
+
+}
