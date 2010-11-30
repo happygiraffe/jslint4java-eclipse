13daf3a20867a144b75797f69f115b8444469709
Dominic Mitchell <dom@happygiraffe.net>
2010-11-30 22:03:30 +0000

    Format properly.

--- com.googlecode.jslint4java.eclipse.ui/src/com/googlecode/jslint4java/eclipse/ui/JSLintUIPlugin.java
+++ com.googlecode.jslint4java.eclipse.ui/src/com/googlecode/jslint4java/eclipse/ui/JSLintUIPlugin.java
@@ -13,46 +13,50 @@ import com.googlecode.jslint4java.eclipse.JSLintPlugin;
  */
 public class JSLintUIPlugin extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "com.googlecode.jslint4java.eclipse.ui"; //$NON-NLS-1$
+    // The plug-in ID
+    public static final String PLUGIN_ID = "com.googlecode.jslint4java.eclipse.ui"; //$NON-NLS-1$
 
-	// The shared instance
-	private static JSLintUIPlugin plugin;
+    // The shared instance
+    private static JSLintUIPlugin plugin;
 
     private IPreferenceStore preferenceStore;
 
-	/**
-	 * The constructor
-	 */
-	public JSLintUIPlugin() {
-	}
+    /**
+     * The constructor
+     */
+    public JSLintUIPlugin() {
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static JSLintUIPlugin getDefault() {
-		return plugin;
-	}
+    /**
+     * Returns the shared instance
+     *
+     * @return the shared instance
+     */
+    public static JSLintUIPlugin getDefault() {
+        return plugin;
+    }
 
     /**
      * Override the default implementation in order to store prefs in the correct scope.
