fb4208eb5fab8c8436d5a479c78758261f42ed0b
Dominic Mitchell <dom@happygiraffe.net>
2010-12-16 08:19:39 +0000

    Clean up the nature.

--- com.googlecode.jslint4java.eclipse/src/com/googlecode/jslint4java/eclipse/builder/JSLintNature.java
+++ com.googlecode.jslint4java.eclipse/src/com/googlecode/jslint4java/eclipse/builder/JSLintNature.java
@@ -19,33 +19,40 @@ import com.googlecode.jslint4java.eclipse.JSLintPlugin;
  */
 public class JSLintNature implements IProjectNature {
 
-    /**
-     * ID of this project nature
-     */
+    /** ID of this project nature. */
     public static final String NATURE_ID = JSLintPlugin.PLUGIN_ID + ".jsLintNature";
 
-    private IProject project;
-
     /**
-     * Add the necessary builder commands for this nature.
+     * Add the JSLint build commands to a project.  If it already has them, no change will be made.
      */
-    public void configure() throws CoreException {
-        List<ICommand> cmds = getCommands();
+    public static void addCommands(IProject project) throws CoreException {
+        List<ICommand> cmds = getCommands(project);
         for (ICommand cmd : cmds) {
             if (cmd.getBuilderName().equals(JSLintBuilder.BUILDER_ID)) {
                 return;
             }
         }
 
-        cmds.add(newCommand(JSLintBuilder.BUILDER_ID));
-        setCommands(cmds);
+        cmds.add(newCommand(project, JSLintBuilder.BUILDER_ID));
+        setCommands(project, cmds);
     }
 
-    /**
-     * Remove the JSLint builder commands.
-     */
-    public void deconfigure() throws CoreException {
-        List<ICommand> cmds = getCommands();
+    /** Return a (mutable) list of commands (builders) in a project. */
+    private static List<ICommand> getCommands(IProject project) throws CoreException {
+        IProjectDescription desc = project.getDescription();
+        return new ArrayList<ICommand>(Arrays.asList(desc.getBuildSpec()));
+    }
+
+    /** Make a new command (builder). */
+    private static ICommand newCommand(IProject project, String builderName) throws CoreException {
+        ICommand cmd = project.getDescription().newCommand();
+        cmd.setBuilderName(builderName);
+        return cmd;
+    }
+
+    /** Remove the JSLint commands from a project. */
+    public static void removeFrom(IProject project) throws CoreException {
+        List<ICommand> cmds = getCommands(project);
         // Separate detection from removal to avoid a ConcurrentModificationException.
         Set<ICommand> togo = new HashSet<ICommand>();
         for (ICommand cmd : cmds) {
@@ -54,31 +61,34 @@ public class JSLintNature implements IProjectNature {
             }
         }
         cmds.removeAll(togo);
-        setCommands(cmds);
+        setCommands(project, cmds);
     }
 
-    /** Return a (mutable) list of commands (builders) in the current project. */
-    private List<ICommand> getCommands() throws CoreException {
+    /** Set the list of commands (builders) for a project. */
+    private static void setCommands(IProject project, List<ICommand> commands) throws CoreException {
         IProjectDescription desc = project.getDescription();
-        return new ArrayList<ICommand>(Arrays.asList(desc.getBuildSpec()));
+        desc.setBuildSpec(commands.toArray(new ICommand[commands.size()]));
+        project.setDescription(desc, null);
+    }
+
+    private IProject project;
+
+    /**
+     * Add the necessary builder commands for this nature.
+     */
+    public void configure() throws CoreException {
+        addCommands(project);
+    }
+
+    /**
+     * Remove the JSLint builder commands.
+     */
+    public void deconfigure() throws CoreException {
+        removeFrom(project);
     }
 
     public IProject getProject() {
         return project;
-    }
-
-    /** Make a new command (builder)*/
-    private ICommand newCommand(String builderName) throws CoreException {
-        ICommand cmd = project.getDescription().newCommand();
-        cmd.setBuilderName(builderName);
-        return cmd;
-    }
-
-    /** Set the list of commands (builders) for this project. */
-    private void setCommands(List<ICommand> commands) throws CoreException {
-        IProjectDescription desc = project.getDescription();
-        desc.setBuildSpec(commands.toArray(new ICommand[commands.size()]));
-        project.setDescription(desc, null);
     }
 
     public void setProject(IProject project) {
