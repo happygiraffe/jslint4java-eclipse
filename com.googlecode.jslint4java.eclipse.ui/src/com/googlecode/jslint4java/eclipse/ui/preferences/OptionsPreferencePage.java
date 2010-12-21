5144ef18c8bc31139ec53e38eb06c97c15511647
Dominic Mitchell <dom@happygiraffe.net>
2010-12-21 08:52:12 +0000

    Show the boolean prefs in a table.

--- com.googlecode.jslint4java.eclipse.ui/src/com/googlecode/jslint4java/eclipse/ui/preferences/OptionsPreferencePage.java
+++ com.googlecode.jslint4java.eclipse.ui/src/com/googlecode/jslint4java/eclipse/ui/preferences/OptionsPreferencePage.java
@@ -1,8 +1,6 @@
 package com.googlecode.jslint4java.eclipse.ui.preferences;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -11,6 +9,14 @@ import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.IntegerFieldEditor;
 import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.jface.viewers.CheckboxTableViewer;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
@@ -26,7 +32,8 @@ import com.googlecode.jslint4java.eclipse.ui.JSLintUIPlugin;
  * This page is used to modify preferences only. They are stored in the preference store that
  * belongs to the main plug-in class. That way, preferences can be accessed directly via the
  * preference store.
- * <p>TODO: trigger a rebuild on change.
+ * <p>
+ * TODO: trigger a rebuild on change.
  */
 public class OptionsPreferencePage extends FieldEditorPreferencePage implements
         IWorkbenchPreferencePage {
@@ -62,14 +69,72 @@ public class OptionsPreferencePage extends FieldEditorPreferencePage implements
      */
     @Override
     public void createFieldEditors() {
-        List<Option> optionsList = new ArrayList<Option>(Arrays.asList(Option.values()));
-        Collections.sort(optionsList, new BooleanFirstOptionComparator());
-        for (Option o : optionsList) {
+        List<Option> booleanOptions = booleanOptions();
+        List<Option> nonBooleanOptions = nonBooleanOptions();
+
+        // This nastiness copied from DecoratorsPreferencePage.
+        Composite parent = getFieldEditorParent();
+
+        Label topLabel = new Label(parent, SWT.NONE);
+        topLabel.setText("Choose your poison.");
+
+        CheckboxTableViewer tableViewer = CheckboxTableViewer.newCheckList(parent, SWT.SINGLE
+                | SWT.TOP | SWT.BORDER);
+        tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+        tableViewer.setLabelProvider(new LabelProvider() {
+            @Override
+            public String getText(Object element) {
+                return ((Option) element).getDescription();
+            }
+        });
+        tableViewer.setContentProvider(new IStructuredContentProvider() {
+            public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+            }
+
+            public void dispose() {
+            }
+
+            // TODO: switch to pref values.
+            public Object[] getElements(Object inputElement) {
+                @SuppressWarnings("unchecked")
+                ArrayList<Option> optionsList = (ArrayList<Option>) inputElement;
+                return optionsList.toArray(new Option[optionsList.size()]);
+            }
+        });
+        tableViewer.setInput(booleanOptions);
+
+        for (Option o : booleanOptions) {
+            // TODO: fetch value from prefs.
+            tableViewer.setChecked(o, false);
+        }
+
+        // Finally, add in the remaining options below.
+        for (Option o : nonBooleanOptions) {
             FieldEditor fe = makeFieldEditor(o);
             if (fe != null) {
                 addField(fe);
             }
         }
+    }
+
+    private List<Option> booleanOptions() {
+        ArrayList<Option> opts = new ArrayList<Option>();
+        for (Option o: Option.values()) {
+            if (o.getType() == Boolean.class) {
+                opts.add(o);
+            }
+        }
+        return opts;
+    }
+
+    private List<Option> nonBooleanOptions() {
+        ArrayList<Option> opts = new ArrayList<Option>();
+        for (Option o: Option.values()) {
+            if (o.getType() != Boolean.class) {
+                opts.add(o);
+            }
+        }
+        return opts;
     }
 
     /*
